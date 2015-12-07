f8cae84f1a8d7add9837d7e718c314b8e89a51fe
Charles Burnham <felixtheslayer@gmail.com>
2015-12-07 10:55:51 -0600

    day 4 part 1 done, part 2 has a stack overflow

---
+++ day_4.hs
@@ -0,0 +1,26 @@
+import System.IO
+import qualified Data.ByteString.Lazy as BL
+import Data.Digest.Pure.MD5     -- pureMD5 package
+import Codec.Binary.UTF8.String -- uft8-string package
+
+-- MD5 from a string
+stringMD5 :: String -> String
+stringMD5 = show . md5 . BL.pack . encode
+
+md5StartsWith :: String -> String -> Bool
+md5StartsWith str = (.) (str==) $ (take 5) . stringMD5
+
+-- part 1
+firstWith5z = firstInt $ md5StartsWith "00000"
+
+-- part 2
+firstWith6z = firstInt $ md5StartsWith "000000"
+
+firstInt :: (String -> Bool) -> String -> Int -> Int
+firstInt testFunc str i = case testFunc (str ++ (show i)) of False -> firstInt testFunc str (i+1)
+                                                             True -> i
+
+main :: IO ()
+main = do 
+  print $ firstWith5z "iwrupvqb" 1
+  print $ firstWith6z "iwrupvqb" 1
