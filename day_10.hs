f38848bed8d0b87dacb4dcdae2a556b564b79f90
Charles Burnham <cburnham@timeips.com>
2015-12-10 11:08:39 -0600

    fix whitespace

--- day_10.hs
+++ day_10.hs
@@ -1,10 +1,10 @@
 startingInput = "3113322113"
 
 countRepeatingChars :: (Integral a, Show a) => Char -> (Maybe Char, a, String) -> (Maybe Char, a, String)
-countRepeatingChars c (Nothing, _, _)        = (Just c, 1, []) 
+countRepeatingChars c (Nothing, _, _)               = (Just c, 1, []) 
 countRepeatingChars c (Just curChar, curCount, str) = if c == curChar
                                                       then (Just curChar, succ curCount, str)
-						      else (Just c, 1, (show curCount) ++ curChar:str)
+                              else (Just c, 1, (show curCount) ++ curChar:str)
 
 finalPrepend :: (Integral a, Show a) => (Maybe Char, a, String) -> String
 finalPrepend (Just c, count, str) = (show count) ++ c:str
@@ -20,4 +20,3 @@ main :: IO ()
 main = do
   print $ length $ repeatElf 40 startingInput
   print $ length $ repeatElf 50 startingInput
-  
